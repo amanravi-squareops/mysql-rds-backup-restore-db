b3ed476550c808613ffb53ac0108b4b4ab6cb70e
amanravi-squareops <aman.ravi@squareops.com>
2024-02-09 10:53:47 +0530

    adding file

--- examples/complete-mysql/main.tf
+++ examples/complete-mysql/main.tf
@@ -1,22 +1,22 @@
 locals {
-  name                             = "mysql"
-  snap                             = true
-  demo                             = "demo"
-  region                           = "us-west-2"
-  family                           = "mysql8.0"
-  environment                      = "prod"
-  mysql_instance_class             = "db.t3.micro"
-  mysql_engine_version             = "8.0.32"
-  major_engine_version             = "8.0"
-  create_namespace                 = true
-  namespace                        = "mysql-test"
-  allowed_security_groups          = ["sg-02313c561173cff42"]
-  vpc_cidr                 = "192.168.0.0/16"
-  current_identity           = data.aws_caller_identity.current.arn
+  name                        = "mysql"
+  snap                        = true
+  demo                        = "demo"
+  region                      = "us-west-2"
+  family                      = "mysql8.0"
+  environment                 = "prod"
+  mysql_instance_class        = "db.t3.micro"
+  mysql_engine_version        = "8.0.32"
+  major_engine_version        = "8.0"
+  create_namespace            = true
+  namespace                   = "mysql-test"
+  allowed_security_groups     = ["sg-02313c561173cff42"]
+  vpc_cidr                    = "192.168.0.0/16"
+  current_identity            = data.aws_caller_identity.current.arn
   manage_master_user_password = false
-  custom_user_password       = "Amanravi12"
-  enable_storage_autoscaling = false
-  mysqldb_permission = true
+  custom_user_password        = "Amanravi12"
+  enable_storage_autoscaling  = false
+  mysqldb_permission          = true
   additional_tags = {
     Owner      = "Organization_Name"
     Expires    = "Never"
@@ -92,7 +92,7 @@ module "kms" {
 module "vpc" {
   source                  = "squareops/vpc/aws"
   name                    = local.name
-  vpc_cidr                = local.vpc_cidr 
+  vpc_cidr                = local.vpc_cidr
   environment             = local.environment
   availability_zones      = ["us-east-1a", "us-east-1b"]
   public_subnet_enabled   = true
@@ -104,13 +104,13 @@ module "vpc" {
 }
 
 module "rds-mysql" {
-  source  = "squareops/rds-mysql/aws"
-  version = "1.1.1"
-  name    = local.name
-  vpc_id                         = module.vpc.vpc_id
-  family     = local.family
-  multi_az   = false
-  subnet_ids                     = module.vpc.database_subnets
+  source                           = "squareops/rds-mysql/aws"
+  version                          = "1.1.1"
+  name                             = local.name
+  vpc_id                           = module.vpc.vpc_id
+  family                           = local.family
+  multi_az                         = false
+  subnet_ids                       = module.vpc.database_subnets
   db_name                          = "testdb"
   environment                      = local.environment
   kms_key_arn                      = module.kms.key_arn
@@ -138,7 +138,7 @@ module "rds-mysql" {
   slack_username                   = "Admin"
   slack_channel                    = "mysql-notification"
   slack_webhook_url                = "https://hooks/xxxxxxxx"
- 
+
 }
 
 
@@ -147,26 +147,26 @@ module "backup_restore" {
   source                 = "../../modules/backup-restore"
   cluster_name           = "prod-eks"
   mysqldb_backup_enabled = false
-  namespace            = local.namespace
-  bucket_provider_type = "s3"
+  namespace              = local.namespace
+  bucket_provider_type   = "s3"
   mysqldb_backup_config = {
     db_username          = module.rds-mysql.db_instance_username
     db_password          = module.rds-mysql.db_instance_password
-    mysql_database_name  = ""                  # always a user defined
-    s3_bucket_region     = "us-west-2"                # always a user defined
-    cron_for_full_backup = "*/3 * * * *"              # always a user defined
+    mysql_database_name  = ""                          # always a user defined
+    s3_bucket_region     = "us-west-2"                 # always a user defined
+    cron_for_full_backup = "*/3 * * * *"               # always a user defined
     bucket_uri           = "s3://my-rds-backup-files/" #always a user defined
     db_endpoint          = replace(module.rds-mysql.db_instance_endpoint, ":3306", "")
   }
 
   mysqldb_restore_enabled = true
-  mysqldb_permission = local.mysqldb_permission
+  mysqldb_permission      = local.mysqldb_permission
   mysqldb_restore_config = {
     db_endpoint      = replace(module.rds-mysql.db_instance_endpoint, ":3306", "")
     db_username      = module.rds-mysql.db_instance_username
     db_password      = module.rds-mysql.db_instance_password
     bucket_uri       = "s3://my-rds-backup-files/mysqldump_20240201_114539.zip" # always a user defined
-    file_name        = "mysqldump_20240201_114539.zip"                         # always a user defined
-    s3_bucket_region = "us-west-2"                                             # always a user defined
+    file_name        = "mysqldump_20240201_114539.zip"                          # always a user defined
+    s3_bucket_region = "us-west-2"                                              # always a user defined
   }
 }
